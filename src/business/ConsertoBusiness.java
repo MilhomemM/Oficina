db3184225587a54863abed2e264efb0cc4a983e7
Delicinha <flavioalves.ti@gmail.com>
2015-11-07 03:08:04 -0300

    ConsertoBusiness e ConsertoController upados mas incompletos

--- src/business/ConsertoBusiness.java
+++ src/business/ConsertoBusiness.java
@@ -1,8 +1,13 @@
 package business;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import model.Conserto;
+import model.Data;
+import model.Funilaria;
+import model.Mecanica;
+import model.Pintura;
 
 public class ConsertoBusiness {
 	private ArrayList<Conserto> banco;
@@ -15,7 +20,125 @@ public class ConsertoBusiness {
 		return this.banco;
 	}
 
+	public int getSize() {
+		return this.banco.size();
+	}
+
 	public boolean adicionar(Conserto novo) {
 		return this.banco.add(novo);
+	}
+
+	public Conserto pesquisarCodigo(String codigo) {
+
+		if (this.banco == null || this.getSize() == 0) {
+			return null;
+		}
+
+		for (int i = 0; i < this.getSize(); i++) {
+			if (this.banco.get(i).getCodigo().equalsIgnoreCase(codigo))
+				;
+			return this.banco.get(i);
+		}
+		return null;
+	}
+
+	public ArrayList<Conserto> pesquisarCliente(String proprietario) {
+		ArrayList<Conserto> resultado = new ArrayList<Conserto>();
+
+		if (this.banco == null || this.getSize() == 0) {
+			return null;
+		}
+		for (int i = 0; i < this.getSize(); i++) {
+
+			if (this.banco.get(i).getVeiculo().getProprietario().getNome().equalsIgnoreCase(proprietario)) {
+				resultado.add(this.banco.get(i));
+			}
+		}
+		return resultado;
+	}
+
+	public ArrayList<Conserto> pesquisarVeiculo(String placaVeiculo) {
+		ArrayList<Conserto> resultado = new ArrayList<Conserto>();
+
+		if (this.banco == null || this.getSize() == 0) {
+			return null;
+		}
+
+		for (int i = 0; i < this.getSize(); i++) {
+
+			if (this.banco.get(i).getVeiculo().getPlaca().equalsIgnoreCase(placaVeiculo)) {
+				resultado.add(this.banco.get(i));
+			}
+		}
+		return resultado;
+	}
+
+	public ArrayList<Conserto> pesquisarData(Date data) {
+		ArrayList<Conserto> resultado = new ArrayList<Conserto>();
+		Data dt = new Data();
+		if (this.banco == null || this.getSize() == 0) {
+			return null;
+		}
+
+		for (int i = 0; i < this.getSize(); i++) {
+
+			if (dt.filtroData(this.banco.get(i).getData()).equals(dt.filtroData(data))) {
+				resultado.add(this.banco.get(i));
+			}
+		}
+
+		return resultado;
+
+	}
+
+	public ArrayList<Conserto> pesquisarTipoServico(String tipoServico) {
+		ArrayList<Conserto> resultado = new ArrayList<Conserto>();
+
+		if (this.banco == null || this.getSize() == 0)
+			return null;
+
+		if (tipoServico.equalsIgnoreCase("Pintura")) {
+			for (int i = 0; i < this.getSize(); i++) {
+				for (int j = 0; j < this.banco.get(i).getServicos().size(); j++) {
+
+					if (this.banco.get(i).getServicos().get(j) instanceof Pintura) {
+						resultado.add(this.banco.get(i));
+					}
+				}
+			}
+		} else if (tipoServico.equalsIgnoreCase("Mecanica")) {
+			for (int i = 0; i < this.getSize(); i++) {
+				for (int j = 0; j < this.banco.get(i).getServicos().size(); j++) {
+
+					if (this.banco.get(i).getServicos().get(j) instanceof Mecanica) {
+						resultado.add(this.banco.get(i));
+					}
+				}
+
+			}
+		} else if (tipoServico.equalsIgnoreCase("Funilaria")) {
+			for (int i = 0; i < this.getSize(); i++) {
+				for (int j = 0; j < this.banco.get(i).getServicos().size(); j++)
+					if (this.banco.get(i).getServicos().get(j) instanceof Funilaria) {
+						resultado.add(this.banco.get(i));
+					}
+			}
+		}
+
+		return resultado;
+	}	
+	
+	public ArrayList<Conserto> pesquisarPreco(double min, double max) {
+		ArrayList<Conserto> resultado = new ArrayList<Conserto>();
+
+		if (this.banco == null || this.getSize() == 0)
+			return null;
+
+		for (int i = 0; i < this.getSize(); i++) {
+			if (this.banco.get(i).getTotal() >= min && this.banco.get(i).getTotal() <= max){
+				resultado.add(this.banco.get(i));
+			}
+		}
+		return resultado;
 	}
 }
--- src/controller/ConsertoController.java
+++ src/controller/ConsertoController.java
@@ -1,12 +1,17 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import business.ConsertoBusiness;
+import model.Conserto;
+import model.Data;
 
 @WebServlet({ "/ConsertoController", "/ConsertoController.do" })
 public class ConsertoController extends HttpServlet {
@@ -38,5 +43,50 @@ public class ConsertoController extends HttpServlet {
 			throws ServletException, IOException {
 
 	}
-
+	
+	private void pesquisa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String dispatcher;
+		String tipoDePesquisa = request.getParameter("tipoPesquisa");
+		String campoDePesquisa = request.getParameter("campoDePesquisa");
+	
+		ConsertoBusiness bancoConserto = (ConsertoBusiness) request.getServletContext().getAttribute("bancoConserto");
+	
+		ArrayList<Conserto> resultado = new ArrayList<Conserto>();
+		Conserto resultadoEspecifico;
+		Data dt = new Data();
+		
+		switch(tipoDePesquisa.toLowerCase()){
+		
+		case "codigo":
+			resultadoEspecifico = bancoConserto.pesquisarCodigo(campoDePesquisa);
+			dispatcher = "conserto-exibir.jsp";
+			break;
+		case "nome":
+			resultado = bancoConserto.pesquisarCliente(campoDePesquisa);
+			dispatcher = "conserto-pesquisar.jsp";
+			break;
+		case "placa":
+			resultado = bancoConserto.pesquisarVeiculo(campoDePesquisa);
+			dispatcher = "conserto-pesquisar.jsp";
+			break;
+		case "data":
+			resultado = bancoConserto.pesquisarData(dt.returnDate(campoDePesquisa));
+			dispatcher = "conserto-pesquisar.jsp";
+			break;
+		case "tiposervico":
+			resultado = bancoConserto.pesquisarTipoServico(campoDePesquisa);
+			dispatcher = "conserto-pesquisar.jsp";
+			break;
+//		case "preco":
+//			resultado = bancoConserto.pesquisarPreco(0, Double.parseDouble(campoDePesquisa));
+//			dispatcher = "conserto-pesquisar.jsp";
+//			break;
+		default: 
+			dispatcher = "Home.jsp";
+		}
+		
+		request.setAttribute("listouConserto", Boolean.TRUE);
+		request.setAttribute("resultadoPesquisa", resultado);
+		request.getRequestDispatcher(dispatcher).forward(request, response);
+	}
 }
