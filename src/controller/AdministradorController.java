217441ef1db9d6555b7754f876a813be177e974f
MilhomemM <nfsmarcelo@gmail.com>
2015-11-22 00:03:23 -0300

    aa

--- src/controller/AdministradorController.java
+++ src/controller/AdministradorController.java
@@ -2,15 +2,8 @@ package controller;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import business.AdministradorBusiness;
 import model.Administrador;
-import model.Cliente;
 import model.Data;
 import model.Usuario;
 
